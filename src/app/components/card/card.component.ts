71387212257caea5bec7007a343837500a48db17
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-23 12:56:53 -0300

    update

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,3 +1,4 @@
+import { InfoPokemons } from './../../models/pokemonData';
 import { Component, OnInit } from '@angular/core';
 import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
@@ -8,14 +9,18 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  public pokemon:PokemonData = {
+  public infoPokemons:InfoPokemons = {
     id: 0,
-    name:'',
     sprites:{
       front_default:''
     },
     types:[]
   }
+
+  pokemons:PokemonData[] = [
+
+  ]
+
   maxRecords = 151
   public limit:number = 10
   public offset:number = 0
@@ -25,95 +30,34 @@ export class CardComponent {
   ){}
 
   ngOnInit():void{
-    this.getPokemon(`?${this.offset}?&{this.limit}`)
+    this.defaultPokemon(0)
+
   }
 
   getPokemon(searchName:string ){
     this.service.getPokemon(searchName).subscribe({
       next:(res: any) => {
 
-        this.pokemon = {
+        this.infoPokemons = {
           id:        res.id,
-          name:      res.name,
           sprites:   res.sprites,
           types:     res.types
         }
         console.log(res)
-        console.log(this.pokemon)
+        console.log(this.infoPokemons)
       }
     })
+  }
 
-<<<<<<< HEAD
-  test1(limit: number){
+  defaultPokemon(limit: number){
     this.service.defaultPokemon(limit).subscribe({
       next:(res: any) => {
-
-        this.pokemon = {
-          id:        res.id,
-          name:      res.name,
-          sprites:   res.sprites,
-          types:     res.types
-        }
+        this.pokemons = res.results
         console.log(res)
-        console.log(this.pokemon)
       }
     })
 
 
-  }
-}
-=======
-
-    const maxRecords = 151
-    const limit = 10
-    let offset = 0;
-
-    // Elementos do DOM
-const pokemonsList = document.getElementById('pokemonsList') as HTMLUListElement
-const loadMoreButton = document.getElementById('loadMore') as HTMLButtonElement
-
-// Função que carrega os Pokémon
-async function loadPokemonItems(offset: number, limit: number): Promise<void> {
-  try {
-    const pokemons = await PokemonService.getPokemons(offset, limit)
-
-    const newHtml = pokemons.map((pokemon: any) => `
-      <li class="pokemon ${pokemon.types.join(' ')}">
-        <span class="number">#${pokemon.id}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-          <ol class="types">
-            ${pokemon.types.map((type: string) => `<li class="type">${type}</li>`).join('')}
-          </ol>
-          <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-        </div>
-      </li>
-    `).join('')
-
-    pokemonsList.innerHTML += newHtml
-  } catch (error) {
-    console.error('Erro ao carregar Pokémon:', error)
-  }
-}
-
-// Event listener do botão
-loadMoreButton.addEventListener('click', () => {
-  offset += limit
-  const qtdRecordNextPage = offset + limit
-
-  if (qtdRecordNextPage >= maxRecords) {
-    const newLimit = maxRecords - offset
-    loadPokemonItems(offset, newLimit)
-    loadMoreButton.parentElement?.removeChild(loadMoreButton)
-  } else {
-    loadPokemonItems(offset, limit)
-  }
-})
-
-// Carrega os primeiros pokémons ao iniciar
-loadPokemonItems(offset, limit)
-
 
   }
->>>>>>> refs/remotes/origin/main
 }
--- src/app/models/pokemonData.ts
+++ src/app/models/pokemonData.ts
@@ -1,5 +1,21 @@
 export type PokemonData = {
   name:string
+  url: string
+  /* id:number
+  sprites: {
+    front_default: string
+  }
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    }
+  }[] */
+
+}
+
+export type InfoPokemons = {
   id:number
   sprites: {
     front_default: string
@@ -11,7 +27,4 @@ export type PokemonData = {
       url: string;
     }
   }[]
-
-
-
 }
